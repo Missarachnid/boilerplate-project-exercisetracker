9b4779dcc6a9bab933f7e4bad44eb413f43fc320
M. Kepler <mmkepler@yahoo.com>
2022-03-16 18:02:40 -0600

    mongoDB & mongoose
    mongoDB and mongoose added, connected and schemas/models written.

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: "false" }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//connect to mongodb
+uri = process.env.MONGO_URI;
 
+mongoose.connect(uri, {useNewUrlParser: true})
+.catch((err) => console.log(err));
 
+//schema setup
+const exerciseSchema = new Schema({
+  username: {type:String, required: true},
+  description: String,
+  duration: Number,
+  date: { type: Date, default: Date.now }
+});
 
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+const userSchema = new Schema({
+  username: {type: String, required: true}
+});
+
+const User = mongoose.model("User", userSchema);
+
+const logSchema = new Schema({
+  username: {type: String, required: true},
+  count: Number,
+  log: [{description: String, duration: Number, date:{ type: Date, default: Date.now } }]
+});
+
+const Log = mongoose.model("Log", logSchema);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
