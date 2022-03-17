252f66b0d3a4d0bde3407b6718436a4066713e59
M. Kepler <mmkepler@yahoo.com>
2022-03-17 16:49:15 -0600

    fix: object version
    Data returned from DB doesn't show version key.

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const bodyParser = require("body-parser");
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const regex = /"([^"]*)"/;
+let moment = require("moment");
+let currentTime = moment().format('ll');
+
 
 app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
 
+//serve HTML file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -26,7 +31,7 @@ const exerciseSchema = new Schema({
   username: {type:String, required: true},
   description: String,
   duration: Number,
-  date: { type: Date, default: Date.now }
+  date: { type: Date }
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
@@ -40,7 +45,8 @@ const User = mongoose.model("User", userSchema);
 const logSchema = new Schema({
   username: {type: String, required: true},
   count: Number,
-  log: [{description: String, duration: Number, date:{ type: Date, default: Date.now } }]
+  log: [{description: String, duration: Number, date:{ type: Date } }],
+
 });
 
 const Log = mongoose.model("Log", logSchema);
@@ -49,7 +55,7 @@ const Log = mongoose.model("Log", logSchema);
 app.post("/api/users", (req,res, next)=> {
   let checkName = req.body.username;
   console.log(checkName);
-  User.findOne({username: checkName}, (err, data) => {
+  User.findOne({username: checkName}).select("-__v").exec((err, data) => {
     if (err){
       res.send("User search error, please try again");
       console.log("Error username search", err);
@@ -60,6 +66,7 @@ app.post("/api/users", (req,res, next)=> {
       res.send({username: data.username, _id: data._id});
     } else {
       //Create new user
+      // - Might have to change the values around so name is first
       let addUser = new User({username: checkName});
       console.log("addUser", addUser);
       addUser.save((err, newUserData) => {
@@ -79,6 +86,8 @@ app.get("/api/users", (req, res) => {
     res.send(allUsersData);
   });
 });
+
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
