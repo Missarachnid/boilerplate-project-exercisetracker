14d0c0ae062d1d0e5a92ef16f87c06f29d9b1e4e
M. Kepler <mmkepler@yahoo.com>
2022-03-17 19:53:58 -0600

    feat: add exercise
    A user can add an exercise if they know the userid number.

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ const bodyParser = require("body-parser");
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const regex = /"([^"]*)"/;
-let moment = require("moment");
-let currentTime = moment().format('ll');
 
 
 app.use(cors());
@@ -31,7 +28,7 @@ const exerciseSchema = new Schema({
   username: {type:String, required: true},
   description: String,
   duration: Number,
-  date: { type: Date }
+  date: String
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
@@ -45,7 +42,7 @@ const User = mongoose.model("User", userSchema);
 const logSchema = new Schema({
   username: {type: String, required: true},
   count: Number,
-  log: [{description: String, duration: Number, date:{ type: Date } }],
+  log: [{description: String, duration: Number, date: String }],
 
 });
 
@@ -54,21 +51,21 @@ const Log = mongoose.model("Log", logSchema);
 //Check for user, create new users
 app.post("/api/users", (req,res, next)=> {
   let checkName = req.body.username;
-  console.log(checkName);
+  //console.log(checkName);
   User.findOne({username: checkName}).select("-__v").exec((err, data) => {
     if (err){
       res.send("User search error, please try again");
-      console.log("Error username search", err);
+      //console.log("Error username search", err);
     }
     //If a user object was returned
     if(data){
-      console.log("User search data", data);
+      //console.log("User search data", data);
       res.send({username: data.username, _id: data._id});
     } else {
       //Create new user
       // - Might have to change the values around so name is first
       let addUser = new User({username: checkName});
-      console.log("addUser", addUser);
+      //console.log("addUser", addUser);
       addUser.save((err, newUserData) => {
         if(err) res.send("User creation error, please try again");
         res.send({username: newUserData.username, _id: newUserData._id })
@@ -85,6 +82,38 @@ app.get("/api/users", (req, res) => {
     //This might not pass, check if it needs to say name then id
     res.send(allUsersData);
   });
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  let userId = req.body[':_id'];
+  let enteredDate = req.body.date;
+  let current
+
+  if(enteredDate.match(/(\d{4})-(\d{2})-(\d{2})/)){
+    current = new Date(enteredDate);
+    enteredDate = current.toDateString();
+  } else {
+    
+    current = new Date();
+    enteredDate = current.toDateString();
+    //console.log("fixed date no", enteredDate);
+  }
+
+  //console.log("all exercise", req.body);
+  //For some reason using findById wouldn't work and returned a null no matter what
+  //Had to use findOne and add the obj to get it to return user
+  User.findOne({ObjectId: userId}).then(data => {
+    //console.log("dataFinish", data);
+    let workout = new Exercise({username: data.username, description: req.body.description, duration: req.body.duration, date: enteredDate});
+    workout.save().then(saveData => {
+      let exerciseInfo = {username: saveData.username, description: saveData.description, duration: saveData.duration, date: saveData.date, _id: saveData['_id']}
+      res.send(saveData);
+    });
+  }).catch(err => {
+    console.log(err);
+    res.send("This user ID doesn't exist. Please try again.");
+  });
+  
 });
 
 
