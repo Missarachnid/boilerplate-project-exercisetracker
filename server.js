d62ee2a5f0a5fe7052b17697311a5b7465453299
M. Kepler <mmkepler@yahoo.com>
2022-03-19 18:13:34 -0600

    fix: removed variable
    removed mystery variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { v4: uuidv4 } = require('uuid');
-const res = require('express/lib/response');
+
 
 app.use(cors());
 app.use(express.static('public'));
