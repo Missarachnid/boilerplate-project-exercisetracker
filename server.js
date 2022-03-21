26520b216c4d1db41d14b776160604ae27e1abb4
M. Kepler <mmkepler@yahoo.com>
2022-03-20 18:42:39 -0600

    started over
    With so many issues with the Free Code Camp testing, I started over to work on passing each test before moving the the next step.

--- server.js
+++ server.js
@@ -51,167 +51,99 @@ const userSchema = new Schema({
 
 const User = mongoose.model("User", userSchema);
 
-  /***********************************************/
- /********** Helper Functions ******************/
-/*********************************************/
-
-
-
-
-
-
-  /*******************************************************/
- /********** Post - "/api/users" Create Users **********/
-/*****************************************************/
-
-app.post("/api/users", (req,res, next)=> {
-  let checkName = req.body.username;
-  let newId = uuidv4();
-
-  User.findOne({username: checkName}).select("-__v").exec((err, data) => {
-    if (err){
-      res.send("User search error, please try again");
-    }
-    //If a user object was returned
-    if(data){
-      res.send({username: data.username, _id: data.id});
-    } else {
-      //Create new user
-      // - Might have to change the values around so name is first
-      let addUser = new User({username: checkName, id: newId});
-      addUser.save((err, newUserData) => {
-        if(err) res.send("User creation error, please try again");
-        res.send({username: newUserData.username, _id: newUserData.id })
-      });
-    }
-  });
-  
-});
 
   /********************************************************/
- /********** Get - "/api/users" - Get user list **********/
-/********************************************************/
+ /********** Post - "/api/users" Create Users ***********/
+/******************************************************/
 
-app.get("/api/users", (req, res) => {
-  User.find({}).select("-__v").exec((err, allUsersData) => {
-    if(err) res.send("There was an error getting user data, try agin.");
-    //This might not pass, check if it needs to say name then id
-    let allUsers = [];
-    for(let el in allUsersData){
-     allUsers.push({username: allUsersData[el].username, _id: allUsersData[el].id});
+app.post("/api/users", (req, res) => {
+  let usernameInput = req.body.username;
+  let newId = uuidv4();
+
+  if(usernameInput === ""){
+    res.send("Username is required");
+  }
+
+  User.findOne({username: usernameInput}, (err, data) => {
+    if(err) return res.send("There was an issue searching for username. Please try again");
+    if(data === null){
+      let newUser = new User({username: usernameInput, id: newId});
+      newUser.save((err, newUserData) => {
+        if(err) return res.send("There was an issue registering new user. Please try again");
+        res.json({username: newUserData.username, _id: newUserData.id})
+      })
+    } else {
+      return res.json({username: data.username, _id: data.id})
+
     }
-    res.send(allUsers);
-  });
+  })
+
 });
 
-  /*******************************************************************************/
- /********** Post - "/api/users/:_id/exercises - Create new Exercises" **********/
-/*******************************************************************************/
+  /*******************************************************/
+ /********** Get - "/api/users"- Get all users **********/
+/*******************************************************/
+app.get("/api/users", (req, res) => {
+  let userArr = [];
+  User.find({}, (err, allUserData) => {
+    if(err) return res.send("There was an error retrieving all users. Please try again.");
+    for(let i in allUserData){
+      userArr.push({username: allUserData[i].username, _id: allUserData[i].id})
+    }
+    return res.send(userArr);
+  })
+});
+
+  /********************************************************************/
+ /********** Post - "/api/users/:id/exercises" Create Users **********/
+/********************************************************************/
 
 app.post("/api/users/:_id/exercises", (req, res) => {
-  let userId = req.body[':_id'];
-  let enteredDate = req.body.date;
-  let current;
+  let userIdInput = req.body[':_id'];
+  let durationInput = req.body.duration;
+  let descriptionInput = req.body.description;
+  let dateInput = req.body.date;
+  let userNameExercise;
+  let dateFormat;
 
-  if(enteredDate.match(/(\d{4})-(\d{2})-(\d{2})/)){
-    current = new Date(enteredDate);
-  } else {
-    
-    current = new Date(Date.now());
+
+//create if date fits date format create date obj, otherwise time is now
+  if(dateInput.match(/(\d{4})-(\d{2})-(\d{2})/)){
+    dateFormat = new Date(dateInput);
+  }else {
+    dateFormat = new Date(Date.now());
   }
 
-  //For some reason using findById wouldn't work and returned a null no matter what
-  //Had to use findOne and add the obj to get it to return user
-  User.findOne({id: userId}).then(data => {
-    let workout = new Exercise(
-      {username: data.username, 
-        description: req.body.description, 
-        duration: req.body.duration, date: current, 
-        id: userId});
-        console.log('workout data', workout);
-    workout.save().then(saveData => {
-      let exerciseInfo = {username: saveData.username, description: saveData.description, duration: saveData.duration, date: saveData.date.toDateString(), _id: saveData.id};
-      console.log("exercise saved", exerciseInfo);
-      res.send(exerciseInfo);
+  //prevent empty inputs
+  if(descriptionInput === ""){
+    res.send("You must enter exercise details. Please try again.");
+  }
+
+  if(durationInput === ""){
+    res.send("You must enter a duration time. Please try again.");
+  }
+  if(userIdInput === ""){
+    res.send("You must enter a user id number. Please try again.");
+  }
+  User.findOne({id: userIdInput}, (err, exerciseData) => {
+    if(err) return res.send("There was an issue saving this exercise. Please try again.");
+    if(!exerciseData){
+      res.send("Incorrect user id. Please try agaain.");
+    }
+    let workout = new Exercise({username: exerciseData.username, description: descriptionInput, duration: durationInput, date: dateFormat, id: userIdInput});
+    workout.save().then((exerciseSaveData) => {
+      res.json({username: exerciseSaveData.username, description: exerciseSaveData.description, duration: exerciseSaveData.duration, date: exerciseSaveData.date.toDateString(), _id: exerciseSaveData.id});
+    }).catch((err) => {
+      console.log("Exercise save error", err);
+      return res.send("Error saving new exercise. please try again.");
     });
-  }).catch(err => {
-    console.log(err);
-    res.send("This user ID doesn't exist. Please try again.");
   });
+
+  console.log(typeof durationInput);
+  console.log('date', dateFormat)
+  console.log("test", userIdInput, durationInput, descriptionInput, dateFormat.toDateString());
   
 })
-;
-
-  /***************************************************************************/
- /********** Get - "/api/users/:_id/exercises" - Get all exercises **********/
-/***************************************************************************/
-
-app.get("/api/users/:_id/exercises", (req, res) => {
-  let requestId = req.params._id;
-  Exercise.find({id: requestId}).select("-__v").exec((err, exerciseData)=> {
-    if(err) res.send("There was an issue finding users exercises, please try again");
-    let allExercises = [];
-    for(let i in exerciseData){
-      allExercises.push({username: exerciseData[i].username, description: exerciseData[i].description, duration: exerciseData[i].duration, date: exerciseData[i].date.toDateString(), _id: exerciseData[i].id })
-    }
-    res.send(allExercises);
-  });
-});
-
-  /***************************************************************************/
- /********** Get - "/api/users/:_id/log" - Get user log *********************/
-/***************************************************************************/
-
-app.get("/api/users/:_id/logs", (req, res) => {
-  let logId = req.params._id;
-  let toQuery = req.query.to;
-  let fromQuery = req.query.from;
-  //let limitQuery = Number(req.query.limit) || 0;
-  let limitQuery = Number(req.query.limit);
-  let count = 0;
-  let arr = [];
-  let finalLog;
-  let searchFilter = {};
-  let tempTo = new Date(toQuery);
-  let tempFrom = new Date(fromQuery);
-
-  if(fromQuery === undefined){
-    searchFilter = {id: logId};
-  } else if(fromQuery.match(/(\d{4})-(\d{2})-(\d{2})/) && toQuery.match(/(\d{4})-(\d{2})-(\d{2})/)){
-    searchFilter = {id: logId, date: {$gt: tempFrom, $lt: tempTo}};
-  } else {
-    return res.send("There is an issue with your query format");
-  }
-
-  Exercise.find(searchFilter).then((thisthing) => {
-    console.log('thisthing', thisthing);
-    for(let l in thisthing){
-    arr.push({description: thisthing[l].description, duration: thisthing[l].duration, date: thisthing[l].date.toDateString()});
-    //console.log("arr", arr);
-    count = count += 1;
-    }
-    console.log('limitQuery', limitQuery);
-
-    //should I add an error return for non numbers for limit query?
-    if(typeof limitQuery === 'number' && limitQuery < count){
-      arr.length = limitQuery;
-      //count = limitQuery;
-    }
-    
-    finalLog = {
-      username: thisthing[0].username,
-      count: count,
-      _id: logId,
-      logs: arr
-    };
-    res.send(finalLog);
-  })
-  
-  
-});
-
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 });
